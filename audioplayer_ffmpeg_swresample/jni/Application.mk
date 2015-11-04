69a6956ae233cc0198b2004fee68a93ed86a56a7
maxmpz <maxmpz@gmail.com>
2015-11-04 23:55:58 +0300

    - forced APP_MODULES

--- audioplayer_ffmpeg_swresample/jni/Application.mk
+++ audioplayer_ffmpeg_swresample/jni/Application.mk
@@ -5,6 +5,8 @@ APP_OPTIM := release
 
 APP_DEBUGGABLE := false
 
+APP_MODULES := libsoxr libswresample # NOTE: this forces static lib build
+
 NDK_TOOLCHAIN_VERSION := clang3.6
 #NDK_TOOLCHAIN_VERSION := 4.9
 #NDK_TOOLCHAIN_VERSION := 4.8
