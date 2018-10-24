2db44e97ee2a91f34b11478e31dcdf40f7f9828f
maxmpz <maxmpz@gmail.com>
2018-10-24 04:19:19 +0300

    - 803

--- jni/build-neon.sh
+++ jni/build-neon.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+NDK_PATH=/opt/android-ndk-r10e
+
 echo Using ANDROID_NDK=$ANDROID_NDK
 
-$ANDROID_NDK/ndk-build -j6 APP_ABI=armeabi-v7a-hard GLOBAL_ARCH_MODE=neon $*
+$ANDROID_NDK/ndk-build -j12 APP_ABI=armeabi-v7a-hard GLOBAL_ARCH_MODE=neon $*
 
