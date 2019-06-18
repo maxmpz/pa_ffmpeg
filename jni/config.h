0edb6f658f3e99c8819b5273eb2e04cd1a0d53b5
maxmpz <maxmpz@gmail.com>
2019-06-18 20:21:57 +0300

    - recent configs

--- jni/config.h
+++ jni/config.h
@@ -4,12 +4,29 @@
 #	include "config-arm64.h"
 #elif HAVE_NEON
 #	include "config-neon.h"
+#	undef HAVE_VFP_ARGS // NOTE: IMPORTANT to propertly handle soft/hard floats for passing params to .S
+#	if _NDK_MATH_NO_SOFTFP
+#		define HAVE_VFP_ARGS 1
+#	else
+#		define HAVE_VFP_ARGS 0
+#	endif
 #else
-#error
+	#error
+#endif
+
+#ifndef PAMP_FFMPEG_CONFIGURATION // This one should be provided by Android.mk
+#define PAMP_FFMPEG_CONFIGURATION ""
 #endif
 
 #undef FFMPEG_CONFIGURATION
-#define FFMPEG_CONFIGURATION ""
+
+#if defined(__clang_major__)
+#define FFMPEG_CONFIGURATION PAMP_FFMPEG_CONFIGURATION" "AV_STRINGIFY(__clang_major__)"."AV_STRINGIFY(__clang_minor__)
+#elif defined(__GNUC_MINOR__)
+#define FFMPEG_CONFIGURATION PAMP_FFMPEG_CONFIGURATION
+#else
+#define FFMPEG_CONFIGURATION PAMP_FFMPEG_CONFIGURATION
+#endif
 
 #undef CONFIG_LIBSOXR
 #define CONFIG_LIBSOXR 1
