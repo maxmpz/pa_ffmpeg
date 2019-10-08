79b296e89060f54d33079c49faf2d47b77eee067
maxmpz <maxmpz@gmail.com>
2019-10-08 08:20:40 +0300

    - 844

--- jni/libavcodec/mpegaudiodec_float.c
+++ jni/libavcodec/mpegaudiodec_float.c
@@ -8,4 +8,9 @@
 //#pragma GCC optimize ("-fno-tree-loop-if-convert")
 #endif
 
+#include "libavutil/log.h"
+
+#undef AV_LOG_ERROR
+#define AV_LOG_ERROR AV_LOG_DEBUG // Reduce log spam
+
 #include "../FFMpeg/libavcodec/mpegaudiodec_float.c"
